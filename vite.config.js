ce884a7e175c216c9635bf63c2f171504196eddf
dhavalshahh <dhaval1504shah@gmail.com>
2025-07-06 19:09:57 +0530

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Coders_Coffee/', // 👈 this must match your GitHub repo name
 })
